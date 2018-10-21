cd5932f640de2effdf50e9e2682f9789732cf6c9
MihaiAlexandruVlad <crocopublicitate.ro@gmail.com>
2018-10-21 10:20:54 +0300

    Initial Commit

---
+++ backtrack pozitii de paritate diferita   de n cifre/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  backtrack pozitii de paritate diferita   de n cifre
+//
+//  Created by Vlad Alexandru Mihai on 10/21/18.
+//  Copyright © 2018 Vlad Alexandru Mihai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
